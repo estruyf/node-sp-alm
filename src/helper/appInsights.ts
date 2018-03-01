fe4ecfa9294e7d5e435bf2ac88a49b19b381c87e
Elio Struyf <info@estruyf.be>
2018-03-01 12:24:20 +0100

    Optimized telemetry

--- src/helper/appInsights.ts
+++ src/helper/appInsights.ts
@@ -1,8 +1,13 @@
 const packageJSON = require('../../package.json');
 import * as appInsights from 'applicationinsights';
 
-const config = appInsights.setup('d87241c1-2091-4d9b-a457-40055d403ce9');
-config.setInternalLogging(false, false);
+const config = appInsights.setup('d87241c1-2091-4d9b-a457-40055d403ce9')
+                          .setAutoCollectConsole(false)
+                          .setAutoCollectDependencies(false)
+                          .setAutoCollectExceptions(false)
+                          .setAutoCollectPerformance(false)
+                          .setAutoCollectRequests(false)
+                          .setInternalLogging(false, false);
 appInsights.start();
 appInsights.defaultClient.commonProperties = {
   version: packageJSON.version
